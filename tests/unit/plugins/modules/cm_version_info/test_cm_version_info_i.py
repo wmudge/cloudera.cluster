b396d702c2f17e1e70c1b2c639a5c7903613ef04
Webster Mudge <wmudge@cloudera.com>
2024-06-06 15:51:46 -0400

    Refactor cm_version_info module to mixins
    Signed-off-by: Webster Mudge <wmudge@cloudera.com>

--- tests/unit/plugins/modules/cm_version_info/test_cm_version_info_i.py
+++ tests/unit/plugins/modules/cm_version_info/test_cm_version_info_i.py
@@ -18,62 +18,57 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
+import logging
 import os
-import pprint
 import pytest
-import unittest
 
 from ansible_collections.cloudera.cluster.plugins.modules import cm_version_info
-from ansible_collections.cloudera.cluster.tests.unit.plugins.modules.utils import (
+from ansible_collections.cloudera.cluster.tests.unit import (
     AnsibleExitJson,
-    AnsibleFailJson,
-    ModuleTestCase,
-    setup_module_args,
 )
 
-
-@unittest.skipUnless(
-    os.getenv("CM_USERNAME"), "Cloudera Manager access parameters not set"
-)
-class TestCMVersionIntegration(ModuleTestCase):
-    def test_host_discovery(self):
-        setup_module_args(
-            {
-                "username": os.getenv("CM_USERNAME"),
-                "password": os.getenv("CM_PASSWORD"),
-                "host": os.getenv("CM_HOST"),
-                "port": "7180",
-                "verify_tls": "no",
-                "debug": "yes",
-            }
-        )
-
-        with pytest.raises(AnsibleExitJson) as e:
-            cm_version_info.main()
-
-        self.assertEquals(e.value.args[0]["cm"]["version"], "7.6.5")
-
-    def test_direct_endpoint(self):
-        setup_module_args(
-            {
-                "username": os.getenv("CM_USERNAME"),
-                "password": os.getenv("CM_PASSWORD"),
-                "url": "https://"
-                + os.getenv("CM_HOST")
-                + ":"
-                + os.getenv("CM_PORT_TLS")
-                + "/api/"
-                + os.getenv("CM_VERSION"),
-                "verify_tls": "no",
-                "debug": "yes",
-            }
-        )
-
-        with pytest.raises(AnsibleExitJson) as e:
-            cm_version_info.main()
-
-        self.assertEquals(e.value.args[0]["cm"]["version"], "7.6.5")
+LOG = logging.getLogger(__name__)
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture
+def conn():
+    conn = dict(username=os.getenv("CM_USERNAME"), password=os.getenv("CM_PASSWORD"))
+
+    if os.getenv("CM_HOST", None):
+        conn.update(host=os.getenv("CM_HOST"))
+
+    if os.getenv("CM_PORT", None):
+        conn.update(port=os.getenv("CM_PORT"))
+
+    if os.getenv("CM_ENDPOINT", None):
+        conn.update(url=os.getenv("CM_ENDPOINT"))
+
+    if os.getenv("CM_PROXY", None):
+        conn.update(proxy=os.getenv("CM_PROXY"))
+
+    return {
+        **conn,
+        "verify_tls": "no",
+        "debug": "yes",
+    }
+
+
+def test_host_discovery(conn, module_args):
+    if "url" in conn:
+        del conn["url"]
+
+    module_args(conn)
+
+    with pytest.raises(AnsibleExitJson) as e:
+        cm_version_info.main()
+
+    assert e.value.cloudera_manager["version"] == "7.6.5"
+
+
+def test_direct_endpoint(conn, module_args):
+    module_args(conn)
+
+    with pytest.raises(AnsibleExitJson) as e:
+        cm_version_info.main()
+
+    assert e.value.cloudera_manager["version"] == "7.6.5"
